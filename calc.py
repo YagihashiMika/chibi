0493de928d33638a1662db38117d614e6d2c35a8
YAGIHASHI Mika <21716097@jwu.ac.jp>
2019-09-30 16:46:36 +0900

    calc

--- calc.py
+++ calc.py
@@ -1,4 +1,10 @@
-def calc(a, b):
-    return a + b
+def calc(s):
+    print('s=', s)
+    nums = map(int, s.split('+'))
+    print('nums=', nums)
+    return sum(nums)
 
-print(calc(1, 2))
+
+print(calc('1'))
+print(calc('1+2'))
+print(calc('1+2+3'))
